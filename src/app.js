e0ec88e0733110200d69a5c06ef4d15f68af0166
Thiago SIlva <53528074+thiagosilvadev@users.noreply.github.com>
2020-10-27 21:39:48 -0300

    added nodemon

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+    res.json({
+        "message": "olá, mundo"
+    })
+})
+
+
+
 
 app.listen(3000, () => {
     console.log("hello world");
-})
+});
